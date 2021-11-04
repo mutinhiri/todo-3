b60d1774ea6142323dcdf6976432f8ac6fa9ea4b
Bunbee <barnabmutinhiri@gmail.com>
2021-11-04 14:07:07 +0200

    Fix linter errors

--- src/functions.js
+++ src/functions.js
@@ -8,82 +8,81 @@ class ToDo {
     ToDo.list.push(this);
     this.getList = () => ToDo.list;
   }
- 
+
   update() {
     if (this.complete) {
       this.complete = false;
-    }else {
+    } else {
       this.complete = true;
     }
   }
- }
+}
  
+// Add items to UI
+function addtoList() {
+  const todoList = document.getElementById('todo-list');
+  todoList.innerHTML = '';
  
- // Add items to UI
- function addtoList() {
-   const todoList = document.getElementById('todo-list');
-   todoList.innerHTML = '';
+  ToDo.list.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('id', item.index);
+    listItem.classList = 'item-container';
+
+    listItem.innerHTML = `
+    <input class="checkbox" type="checkbox">
+    <span>${item.description}</span>
+    <textarea class="text-area" maxlength="30">${item.description}</textarea>
+    <button class="cancel-btn">u</button>
+    `;
+
+    todoList.appendChild(listItem);
+
+    const checkbox = listItem.querySelector('input');
+    const text = listItem.querySelector('span');
+    const textInput = listItem.querySelector('textarea');
+    const deleteButton = listItem.querySelector('button');
+
+    // Update
+    checkbox.addEventListener('change', () => {
+      const index = parseInt(listItem.id, 10);
+      ToDo.list[index].update();
+      text.classList.toggle('complete');
+      textInput.classList.toggle('complete');
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+    });
  
-   ToDo.list.forEach((item) => {
-     const listItem = document.createElement('li');
-     listItem.setAttribute('id', item.index);
-     listItem.classList = 'item-container';
+    // Edit functionality
+    text.addEventListener('click', () => {
+      text.style.display = 'none';
+      textInput.classList.toggle('edit-item');
+    });
+
+    textInput.addEventListener('keydown', (e) => {
+      text.innerHTML = textInput.value;
+      const index = parseInt(listItem.id, 10);
+      ToDo.list[index].description = text.innerHTML;
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+      if (e.code === 'Enter') {
+        text.style.display = 'block';
+        textInput.classList.toggle('edit-item');
+      }
+    });
  
-     listItem.innerHTML = `
-     <input class="checkbox" type="checkbox">
-     <span>${item.description}</span>
-     <textarea class="text-area" maxlength="30">${item.description}</textarea>
-     <button class="cancel-btn">u</button>
-     `;
+    // Delete functionality
+    deleteButton.addEventListener('click', () => {
+      const index = parseInt(listItem.id, 10);
+      ToDo.list = ToDo.list.filter((item) => item !== ToDo.list[index]);
+      ToDo.list.forEach((item, i) => { item.index = i; });
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+      addtoList();
+    });
  
-     todoList.appendChild(listItem);
- 
-     const checkbox = listItem.querySelector('input');
-     const text = listItem.querySelector('span');
-     const textInput = listItem.querySelector('textarea');
-     const deleteButton = listItem.querySelector('button');
- 
-     // Update
-     checkbox.addEventListener('change', () => {
-       const index = parseInt(listItem.id, 10);
-       ToDo.list[index].update();
-       text.classList.toggle('complete');
-       textInput.classList.toggle('complete');
-       localStorage.setItem('todoList', JSON.stringify(ToDo.list));
-     });
- 
-     // Edit functionality
-     text.addEventListener('click', () => {
-       text.style.display = 'none';
-       textInput.classList.toggle('edit-item');
-     });
- 
-     textInput.addEventListener('keydown', (e) => {
-       text.innerHTML = textInput.value;
-       const index = parseInt(listItem.id, 10);
-       ToDo.list[index].description = text.innerHTML;
-       localStorage.setItem('todoList', JSON.stringify(ToDo.list));
-       if (e.code === 'Enter') {
-         text.style.display = 'block';
-         textInput.classList.toggle('edit-item');
-       }
-     });
- 
-     // Delete functionality
-     deleteButton.addEventListener('click', () => {
-       const index = parseInt(listItem.id, 10);
-       ToDo.list = ToDo.list.filter((item) => item !== ToDo.list[index]);
-       ToDo.list.forEach((item, i) => { item.index = i; });
-       localStorage.setItem('todoList', JSON.stringify(ToDo.list));
-       addtoList();
-     });
- 
-     if (item.complete) {
-       checkbox.checked = true;
-       text.classList = 'complete';
-     }
-   });
- }
+    if (item.complete) {
+      checkbox.checked = true;
+      text.classList = 'complete';
+    }
+  });
+}
  
  // Add functionality
  function add(e) {
